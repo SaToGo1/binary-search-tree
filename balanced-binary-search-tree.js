5d60f8b9ac0ef51d92c70bc813ab598819ac9d31
SaToGo <stoledano97@gmail.com>
2023-02-09 18:53:01 +0100

    add delete method, > > case 3 still in process, right now it makes > a infinite loop in the tree

--- balanced-binary-search-tree.js
+++ balanced-binary-search-tree.js
@@ -75,6 +75,103 @@ class BalancedBinaryTree{
             return this.insert(data, node.right);
         }
     }
+
+    delete(data, node=this.root, previousNode=null){
+
+        if(node === null){
+            return 'Not in Tree';
+        }
+
+        if(data === node.data){
+            //delete
+            this._deleteCases(node, previousNode);
+            return 'deleted';
+        }
+
+        if(data < node.data){
+            return this.delete(data, node.left, node);
+        }
+
+        if(data > node.data){
+            return this.delete(data, node.right, node);
+        }
+    }
+
+    _deleteCases(nodeToDelete, previousNode){
+
+        //CASE 1. node to delete is a leaf, just delete the node.
+        if( ( nodeToDelete.right === null ) && ( nodeToDelete.left === null )){
+            //child bigger than father, chils is in the right of father.
+            if(nodeToDelete.data > previousNode.data){
+                previousNode.right = null;
+            }else{
+            //child smaller than father, child is in the left of father.
+                previousNode.left = null;
+            }
+        }
+
+        //CASE 2. node to delete has only 1 child
+        if( ( ( nodeToDelete.right === null ) && ( nodeToDelete.left != null ) ) ||  ( ( nodeToDelete.right != null ) && ( nodeToDelete.left === null ) ) ){
+            //child bigger than father, child is in the right of father.
+            //
+            if(nodeToDelete.data > previousNode.data ){
+                //node to delete has its child in right
+                if(nodeToDelete.left === null){
+                    previousNode.right = nodeToDelete.right;
+                }else{
+                //node to delete has its child in left
+                    previousNode.right = nodeToDelete.left;
+                }
+            }else{
+            //child smaller than father, child is in the left of father.
+                //node to delete has its child in right
+                if(nodeToDelete.left === null){
+                    previousNode.left = nodeToDelete.right;
+                }else{
+                //node to delete has its child in left
+                    previousNode.left = nodeToDelete.left;
+                }
+            }
+        }
+
+        //CASE 3. node to delete has right and left childs.
+        // We search for the node more to the left(smallest) and replace node to delete with
+        // the smallest node
+        if( ( nodeToDelete.right != null ) && ( nodeToDelete.left != null )){
+            let nextBiggestNode = this._NextBiggest(nodeToDelete);
+            console.log(`next biggest: ${nextBiggestNode.data}`);
+            console.log(`previous node data: ${previousNode.data}`);
+            
+            //child bigger than father, chils is in the right of father.
+            if(nodeToDelete.data > previousNode.data){
+                nextBiggestNode.left = nodeToDelete.left;
+                nextBiggestNode.right = nodeToDelete.right;
+                previousNode.right = nextBiggestNode;
+            }else{
+            //child smaller than father, child is in the left of father.
+                nextBiggestNode.left = nodeToDelete.left;
+                // if() // INFINITE LOOP ON TREE :(
+                nextBiggestNode.right = nodeToDelete.right;
+                previousNode.left = nextBiggestNode;
+            }
+        }
+    }
+
+    _NextBiggest(node){
+        // from a node X the next biggest node in the tree is going 1 to the right
+        // and fom there going to the smallest of that subtree.
+        let subtree = node.right;
+        return this._smallestNode(subtree);
+    }
+
+    _smallestNode(node){
+        if(node.left === null){
+            return node;
+        }
+        else{
+            return this._smallestNode(node.left);
+        }
+    }
 }
 
 module.exports = BalancedBinaryTree
--- main.js
+++ main.js
@@ -1,44 +1,89 @@
 const Tree = require('./balanced-binary-search-tree');
 
 
+const BASE = false;
+const SEARCH = false;
+const INSERT = false;
+const DELETE = true;
+
 let array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let array2 = [0, 1, 2, 3];
 let array3 = [0, 1, 2];
 let array4 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-let tree = new Tree(array1);
-console.log(`From array1: ${array1} make: `)
-tree.prettyPrint();
+if(BASE){
+    let tree = new Tree(array1);
+    console.log(`From array1: ${array1} make: `)
+    tree.prettyPrint();
 
-let tree2 = new Tree(array2);
-console.log(`From array2: ${array2} make: `)
-tree2.prettyPrint();
+    let tree2 = new Tree(array2);
+    console.log(`From array2: ${array2} make: `)
+    tree2.prettyPrint();
 
-let tree3 = new Tree(array3);
-console.log(`From array3: ${array3} make: `)
-tree3.prettyPrint();
+    let tree3 = new Tree(array3);
+    console.log(`From array3: ${array3} make: `)
+    tree3.prettyPrint();
+}
 
+//Default
 let tree4 = new Tree(array4);
 console.log(`From array4: ${array4} make: `)
 tree4.prettyPrint();
 
-let node = tree4.search(6345);
-console.log(node);
+// SEARCH
+if(SEARCH){
+    let node = tree4.search(6345);
+    console.log(node);
+}
 
-console.log(tree4.insert(324));
-console.log(tree4.insert(323));
-tree4.prettyPrint();
+// INSERT
+if(INSERT){
+    console.log(tree4.insert(324));
+    console.log(tree4.insert(323));
+    tree4.prettyPrint();
 
-console.log(tree4.insert(322));
-console.log(tree4.insert(321));
-tree4.prettyPrint();
+    console.log(tree4.insert(322));
+    console.log(tree4.insert(321));
+    tree4.prettyPrint();
 
-let tree5 = new Tree([50]);
-tree5.insert(30);
-tree5.insert(20);
-tree5.insert(40);
-tree5.insert(70);
-tree5.insert(60);
-tree5.insert(80);
-tree5.prettyPrint();
+    let tree5 = new Tree([50]);
+    tree5.insert(30);
+    tree5.insert(20);
+    tree5.insert(40);
+    tree5.insert(70);
+    tree5.insert(60);
+    tree5.insert(80);
+    tree5.prettyPrint();
+}
 
+
+// DELETE
+if(DELETE){
+    console.log(' ');
+    console.log('DELETE');
+    console.log(' ');
+
+    tree6 = new Tree([50, 30, 20, 40, 70, 60, 80]);
+    console.log('EXAMPLE 1')
+    tree6.prettyPrint();
+    console.log('delete 20');
+    tree6.delete(20);
+    tree6.prettyPrint();
+
+    tree7 = new Tree([50, 30, 20, 40, 70, 60, 80]);
+    tree7.insert(49);
+    tree7.insert(48);
+    tree7.insert(47);
+    console.log('EXAMPLE 2')
+    tree7.prettyPrint();
+    console.log('delete 49');
+    tree7.delete(49);
+    tree7.prettyPrint();
+
+    console.log('EXAMPLE 3')
+    tree8 = new Tree([50, 30, 20, 40, 70, 60, 80]);
+    tree8.prettyPrint();
+    console.log('delete 30');
+    tree8.delete(30);
+    // tree8.prettyPrint();
+}
